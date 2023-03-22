db361027839e3d28675339240ba7f9a7673d160b
ClaudioLeone <claleone92@gmail.com>
2023-03-22 14:40:56 +0100

    Completed 'MILESTONE 0'

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// MILESTONE 0:
+// [X] Creare l’array di oggetti con le informazioni fornite.
+
+// MILESTONE 1:
+// [] Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+// MILESTONE 2:
+// [] Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// BONUS 1:
+// [] Trasformare la stringa foto in una immagine effettiva
+
+// BONUS 2:
+// [] Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+
+const teamArray = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'	    
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+console.log(teamArray);
