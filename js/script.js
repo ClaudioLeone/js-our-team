93237ab95e8343574f66bc88c638b93253b06b15
ClaudioLeone <claleone92@gmail.com>
2023-03-22 16:03:45 +0100

    Settled DOM for cards creation via JS, implemented bootstrap for card graphics, implemented card creation and automatic adding in DOM

--- js/script.js
+++ js/script.js
@@ -8,56 +8,67 @@
 // [X] Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 // BONUS 1:
-// [] Trasformare la stringa foto in una immagine effettiva
+// [X] Trasformare la stringa foto in una immagine effettiva
 
 // BONUS 2:
-// [] Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+// [X] Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
 
 const teamArray = [
-    {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg'	    
-    },
-    {
-        name: 'Angela Caroll',
-        role: 'Chief Editor',
-        photo: 'angela-caroll-chief-editor.jpg'
-    },
-    {
-        name: 'Walter Gordon',
-        role: 'Office Manager',
-        photo: 'walter-gordon-office-manager.jpg'
-    },
-    {
-        name: 'Angela Lopez',
-        role: 'Social Media Manager',
-        photo: 'angela-lopez-social-media-manager.jpg'
-    },
-    {
-        name: 'Scott Estrada',
-        role: 'Developer',
-        photo: 'scott-estrada-developer.jpg'
-    },
-    {
-        name: 'Barbara Ramos',
-        role: 'Graphic Designer',
-        photo: 'barbara-ramos-graphic-designer.jpg'
-    }
-]
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photoDir: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photoDir: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photoDir: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    photoDir: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    photoDir: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photoDir: "barbara-ramos-graphic-designer.jpg",
+  },
+];
 
 console.log(teamArray);
 
-const list = document.querySelector(".membersList");
+const list = document.querySelector(".row");
 let listElem = "";
 
 for (let i = 0; i < teamArray.length; i++) {
-    const currMember = teamArray[i];
-    listElem += `
-        <li style="border: 1px solid black">
-            ${currMember.name} &nbsp; ${currMember.role} &nbsp; ${currMember.photo}
-        </li>
+  const currMember = teamArray[i];
+  listElem += `
+        <div class="col">
+            <div class="card">
+                <div class="card-body flex flex-dir-col">
+                
+                <h5 class="card-title">${currMember.name}</h5>
+
+                <h6 class="card-subtitle mb-3 text-muted"><i>${currMember.role}</i></h6>
+
+                <img class="mb-3" src="img/${currMember.photoDir}" alt="${currMember.photoDir}">
+
+                <a href="#" class="card-link">Vedi dettagli</a>
+                </div>
+            </div>
+        </div>
     `;
 }
 
-list.innerHTML += listElem
+list.innerHTML += listElem;
